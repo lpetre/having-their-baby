4e65ae08ce7dfa839d265b5fe7027848f778099a
Luke Petre <lpetre@gmail.com>
2020-02-12 17:47:05 +0000

    Enable PWA manifest & ServiceWorker

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -8,6 +8,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -30,8 +37,20 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Is someone having a baby?`,
+        short_name: `havingtheirbaby.com`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `browser`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
